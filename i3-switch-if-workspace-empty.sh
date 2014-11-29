9f0eb708e646d4d93b594f7787989a06c2ccc2ba
Tasos Bitsios <tas@undesk.co>
2014-11-29 23:37:31 +0000

    bug fix: was looking at second level instead of top-level containers

--- i3-switch-if-workspace-empty.sh
+++ i3-switch-if-workspace-empty.sh
@@ -11,7 +11,11 @@ fi
 
 active_workspace=$( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' )
 
-toplevel_containers=$( i3-msg -t get_tree | jq '(.nodes[].nodes[].nodes[] | select(.type=="workspace") | select(.num=='"$active_workspace"') | .nodes[].nodes | length) // 0' )
+echo active $active_workspace
+
+toplevel_containers=$( i3-msg -t get_tree | jq '(.nodes[].nodes[].nodes[] | select(.type=="workspace") | select(.num=='"$active_workspace"') | .nodes | length) // 0' )
+
+echo windows $toplevel_containers
 
 if [ "$toplevel_containers" -eq "0" ]; then
     i3-msg workspace $new_workspace
